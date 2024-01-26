472594582286889ad6d41f5dfadd2dad2ca5f502
mansooriabbas <mansoori.abbas@outlook.com>
2024-01-26 21:42:45 +0100

    added html and styling

---
+++ script.js
@@ -0,0 +1,5 @@
+const name = document.querySelector(".name");
+const userName = document.querySelector(".username");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+const confirmPassword = document.querySelector(".confirm-password");
